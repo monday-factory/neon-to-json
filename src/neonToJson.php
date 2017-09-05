091315a7ea5607e9db16cd6ac6391b5919d8c866
Pavel Janda <me@paveljanda.com>
2017-09-05 21:17:21 +0200

    neonToJson

---
+++ src/neonToJson.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Nette\Neon\Neon;
+
+if (!isset($argv[1])) {
+	die("\nusage: php neonToJson.php <fileToConvert.neon>\n\n");
+}
+
+$neonFile = $argv[1];
+
+if (!file_exists($neonFile)) {
+	die("\nFile $neonFile does not exist\n\n");
+}
+
+$outputFile = str_replace('.neon', '.json', $neonFile);
+
+$content = Neon::decode(file_get_contents($neonFile));
+file_put_contents($outputFile, json_encode($content, JSON_PRETTY_PRINT));
